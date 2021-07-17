04945fdcd326ec07ad73397064073734a23a1328
David Coles <coles.david@gmail.com>
2021-07-17 15:07:03 -0700

    Print fortune using fortune command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::Write;
+use std::process::Command;
+
+const FORTUNE: &str = "fortune";
+
 fn main() {
-    println!("Hello, world!");
+    println!("[FORTUNE COOKIE]");
+    println!("{}", fortune());
+}
+
+/// Get a fortune cookie.
+fn fortune() -> String {
+    let output = Command::new(FORTUNE)
+        .arg("-s")  // Short fortunes only
+        .output()
+        .expect("Failed to get fortune");
+
+        String::from_utf8(output.stdout).expect("Failed to decode output")
 }
