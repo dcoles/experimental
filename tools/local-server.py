3cc49b8ee16b7fc1103329f116975388adfb2e76
David Coles <dcoles@dcoles.net>
2024-02-02 23:06:24 -0800

    local-server: Fix header sending
    There is unfortunately no way to set additional headers when using
    `SimpleHTTPRequestHandler`, so we have to implement everything ourselves.
    
    The `translate_path` method is taken directly from `SimpleHTTPRequestHandler`.

--- tools/local-server.py
+++ tools/local-server.py
@@ -5,25 +5,103 @@ Local HTTP server for development.
 """
 
 import argparse
-from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 import logging
+import os
+import posixpath
+import shutil
+import urllib
 
 DEFAULT_HOST = '127.0.0.1'
 DEFAULT_PORT = 8000
 
 
-class RequestHandler(SimpleHTTPRequestHandler):
+class RequestHandler(BaseHTTPRequestHandler):
     extensions_map = {
+        '.html': 'text/html; charset=UTF-8',
+        '.txt': 'text/plain',
         '.wgsl': 'text/wgsl',
         '.js': 'text/javascript',
         '.mjs': 'text/javascript',
     }
 
-    def do_GET(self) -> None:
-        # Browser should never cache responses
-        self.headers['Cache-Control'] = 'no-store'
+    # Browser should never cache responses
+    cache_control = 'no_cache'
 
-        return super().do_GET()
+    cross_origin_opener_policy = 'same-origin'
+    cross_origin_embedder_policy = 'require-corp'
+
+    def do_GET(self):
+        """Handle GET requests"""
+        f = self.send_head()
+        if f:
+            shutil.copyfileobj(f, self.wfile)
+            f.close()
+
+    def do_HEAD(self):
+        """Handle HEAD requests"""
+        f = self.send_head()
+        if f:
+            f.close()
+
+    def send_head(self):
+        """Common behaviour between GET/HEAD"""
+        path = self.translate_path(self.path)
+        try:
+            f = open(path, 'rb')
+        except FileExistsError:
+            self.send_error(404, 'Not Found')
+
+        for (suffix, content_type) in self.extensions_map.items():
+            if path.endswith(suffix):
+                break
+        else:
+            content_type = 'application/octet-stream'
+
+        st = os.fstat(f.fileno())
+
+        self.send_response(200)
+        self.send_header('Content-Type', content_type)
+        self.send_header('Content-Length', str(st.st_size))
+        if self.cache_control:
+            self.send_header('Cache-Control', self.cache_control)
+        if self.cross_origin_opener_policy:
+            self.send_header('Cross-Origin-Opener-Policy', self.cross_origin_opener_policy)
+        if self.cross_origin_embedder_policy:
+            self.send_header('Cross-Origin-Embedder-Policy', self.cross_origin_embedder_policy)
+        self.end_headers()
+
+        return f
+
+    def translate_path(self, path):
+        """Translate a /-separated PATH to the local filename syntax.
+
+        Components that mean special things to the local file system
+        (e.g. drive or directory names) are ignored.  (XXX They should
+        probably be diagnosed.)
+
+        """
+        # abandon query parameters
+        path = path.split('?',1)[0]
+        path = path.split('#',1)[0]
+        # Don't forget explicit trailing slash when normalizing. Issue17324
+        trailing_slash = path.rstrip().endswith('/')
+        try:
+            path = urllib.parse.unquote(path, errors='surrogatepass')
+        except UnicodeDecodeError:
+            path = urllib.parse.unquote(path)
+        path = posixpath.normpath(path)
+        words = path.split('/')
+        words = filter(None, words)
+        path = os.getcwd()
+        for word in words:
+            if os.path.dirname(word) or word in (os.curdir, os.pardir):
+                # Ignore components that are not a simple file/directory name
+                continue
+            path = os.path.join(path, word)
+        if trailing_slash:
+            path += '/'
+        return path
 
 
 def parse_args() -> argparse.Namespace:
