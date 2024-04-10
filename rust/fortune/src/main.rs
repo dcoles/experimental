a4be554fa215e4464420dbe97ecb30ca68e90cb4
David Coles <dcoles@dcoles.net>
2024-04-10 01:44:23 -0700

    rust/fortune: Add SVG mode

--- rust/fortune/src/main.rs
+++ rust/fortune/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::io;
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::net::{TcpListener, TcpStream};
 use std::path::Path;
 use std::time::Duration;
@@ -10,6 +11,7 @@ use rand::seq::SliceRandom;
 const ADDR: &str = "127.0.0.1:5000";
 const FORTUNES: &str = "fortunes";
 const REFRESH: Duration = Duration::from_secs(30);
+const MAX_READ: u64 = 1024;
 
 fn main() {
     let fortunes = fortunes_from_file(FORTUNES).expect("Failed to read fortunes");
@@ -50,18 +52,56 @@ fn handle_stream(mut stream: TcpStream, fortunes: &[String]) -> io::Result<()> {
     let mut rng = rand::thread_rng();
     let picked_fortune = fortunes.choose(&mut rng).expect("No fortunes available");
 
-    writeln!(
-        stream,
-        concat!(
-            "HTTP/1.0 200 OK\r\n",
-            "Content-Type: text/plain; charset=UTF-8\r\n",
-            "Refresh: {}\r\n",
-            "\r\n",
-            "{}"
-        ),
-        REFRESH.as_secs(),
-        picked_fortune
-    )?;
+    let reader = BufReader::new(stream.try_clone()?);
+
+    let mut line = String::new();
+    reader.take(MAX_READ).read_line(&mut line)?;
+
+    let request: Vec<&str> = line.trim_end().splitn(3, " ").collect();
+    if request.len() != 3 || request[0] != "GET" {
+        return Ok(());
+    }
+
+    match request[1] {
+        "/fortune.svg" => {
+            // SVG fortune
+            writeln!(
+                stream,
+                concat!(
+                    "HTTP/1.0 200 OK\r\n",
+                    "Content-Type: image/svg+xml; charset=UTF-8\r\n",
+                    "\r\n",
+                    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n",
+                    "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\" viewBox=\"0 0 500 125\">\n",
+                    "  <switch>\n",
+                    "    <foreignObject x=\"0\" y=\"0\" width=\"100%\" height=\"125px\">\n",
+                    "      <xhtml:div style=\"display: flex; height: 125px; justify-content: center; align-items: center;\">\n",
+                    "        <xhtml:span style=\"padding: 1em; text-align: justify; font-size: 10pt; font-family: Segoe Print, Bradley Hand ITC, sans-serif; color: #000; white-space: pre-wrap;\"><![CDATA[{}]]></xhtml:span>\n",
+                    "      </xhtml:div>\n",
+                    "    </foreignObject>\n",
+                    "    <text x=\"50%\" y=\"50%\" text-anchor=\"middle\">Your SVG viewer cannot display HTML</text>\n",
+                    "  </switch>\n",
+                    "</svg>\n"
+                ),
+                picked_fortune
+            )?;
+        },
+        _ => {
+            // Standard text fortune
+            writeln!(
+                stream,
+                concat!(
+                    "HTTP/1.0 200 OK\r\n",
+                    "Content-Type: text/plain; charset=UTF-8\r\n",
+                    "Refresh: {}\r\n",
+                    "\r\n",
+                    "{}"
+                ),
+                REFRESH.as_secs(),
+                picked_fortune
+            )?;
+        },
+    }
 
     Ok(())
 }
